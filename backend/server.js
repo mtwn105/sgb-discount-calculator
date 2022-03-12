0279b4e4a88b21d4e3d93a2261b44d7f893731f6
Amit Wani <mtwn105@gmail.com>
2022-03-12 13:55:33 +0530

    date now

--- backend/server.js
+++ backend/server.js
@@ -157,6 +157,7 @@ updateData = async () => {
       info.goldPriceUsd = goldPrice;
       info.goldPriceInr = (goldPrice * usdInrPrice * 1.1) / 31.1035;
       info.usdInrPrice = usdInrPrice;
+      info.lastUpdatedDate = Date.now;
     }
 
     const data = await Promise.all(
